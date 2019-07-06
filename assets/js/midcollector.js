eedcdd3f4f3c26a036526547d1359bce75672400
Randy Gonzalez Santana <ing.randygonzalez@gmail.com>
2019-07-05 20:29:22 -0400

    + midcollector.js

--- assets/js/midcollector.js
+++ assets/js/midcollector.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
    console.log('Add images to payment option...');
+   Ecwid.OnPageLoad.add(function(page) {
+        console.log("MidCollector page load handler: ", page.type);
+  });
 });
